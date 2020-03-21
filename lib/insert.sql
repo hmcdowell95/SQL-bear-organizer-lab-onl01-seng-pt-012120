3cb602648e8db435ecb44bf3e40e748e46ecb8c3
Harley McDowell <hmcdowell95@gmail.com>
2020-03-21 02:46:17 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES 
+(1, "Mr.Chocolate", 10, "male", "brown", "playful", 1), 
+(2, "Rowdy", 10, "male", "brown", "playful", 1), 
+(3, "Tabitha", 10, "female", "brown", "playful", 1), 
+(4, "Sergeant Brown", 10, "male", "brown", "playful", 1), 
+(5, "Melissa", 10, "female", "brown", "playful", 1), 
+(6, "Grinch", 10, "male", "brown", "bashful", 1), 
+(7, "Wendy", 10, "female", "brown", "playful", 1), 
+(8, NULL, 10, "male", "brown", "playful", 1)
